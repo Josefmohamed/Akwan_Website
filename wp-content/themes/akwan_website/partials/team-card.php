6cb12698d5e38f74ec605987c6455f8a405f3307
dell <hendaoye.201250@gamil.com>
2025-02-12 04:22:54 +0200

    feat : team_block cbt

--- wp-content/themes/akwan_website/partials/team-card.php
+++ wp-content/themes/akwan_website/partials/team-card.php
@@ -1,15 +1,27 @@
 <?php
 $post_id = @$args['post_id'] ?: get_the_ID();
-$post_title = get_the_title($post_id);
-$post_permalink = get_the_permalink($post_id);
+//$post_title = get_the_title($post_id);
+//$post_permalink = get_the_permalink($post_id);
+
+$member_image = get_field('member_image', $post_id);
+$member_name = get_field('member_name', $post_id);
+$member_job = get_field('member_job', $post_id);
+$member_description = get_field('member_description', $post_id);
 ?>
-<a class="mentors-card" href="<?= $post_permalink ?>">
-  <picture class="aspect-ratio">
+<div class="team-cards">
+  <picture class="aspect-ratio member-image">
     <?= \Theme\Helpers::get_post_thumbnail($post_id, 'img-384-384') ?>
   </picture>
-  <?php if ($post_title) { ?>
-    <h3 class="mentors-card-title"><?= $post_title ?></h3>
-  <?php } ?>
-  <button class="mentors-card-link theme-cta-link"><?= __('Full Bio', 'akwan_website') ?></button>
-</a>
+  <div class="member-content">
+    <?php if ($member_name) { ?>
+    <h3><?= $member_name ?></h3>
+    <?php } ?>
+    <?php if ($member_job) { ?>
+      <h4><?= $member_job ?></h4>
+    <?php } ?>
+    <?php if ($member_description) { ?>
+    <div class="member-description"><?= $member_description ?></div>
+    <?php } ?>
+  </div>
+</div>
 
